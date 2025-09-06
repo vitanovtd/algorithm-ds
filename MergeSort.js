00e1437cd044fe1d5d2b8d42671064d0a068e1a4
Tihomir Vitanov <vitanovtd@gmail.com>
2025-09-06 12:06:18 +0200

    mergeSort

---
+++ MergeSort.js
@@ -0,0 +1,36 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = arr.length / 2;
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  // recursive sort each half
+
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  return merge(sortedLeft, sortedRight);
+}
+
+function merge(left, right) {
+  const result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  return result.concat(left.slice(i).concat(right.slice(j)));
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
